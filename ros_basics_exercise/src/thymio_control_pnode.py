7bbd748c99a3e5ab4bcc54c4499ebbb4f9233171
student <mikqs>
2021-03-14 08:42:31 -0700

    working waypoint algo!

--- ros_basics_exercise/src/thymio_control_pnode.py
+++ ros_basics_exercise/src/thymio_control_pnode.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import rospy
 import math
 from ros_basics_msgs.srv import CurrentWaypoint
@@ -8,21 +7,24 @@ from ros_basics_msgs.srv import CheckWaypointReached
 from ros_basics_msgs.msg import SimplePoseStamped
 from ros_basics_msgs.msg import SimpleVelocities
 
-Kp_v = 0.4
-Kp_w = 2
-Ki_v = 0.1
-Ki_w = 0.2
-Kd_v = 0.2
-Kd_w = 0.3
+Kp_v = 1.2
+Kp_w = 2.5
+Ki_v = 0.05
+Ki_w = 0.05
+Kd_v = 0.06
+Kd_w = 0.06
 dt = 0.1 #10 Hz is dt=0.1 s 
+
+pose_msg = SimplePoseStamped()
+curr_pose = None
 
 def get_pose(_data):
     global curr_pose
-    #rospy.loginfo(_data.pose)
+    global pose_msg
+    pose_msg = _data
     curr_pose = _data.pose
 
 def set_velocity(v,w):
-    pub = rospy.Publisher('set_velocities', SimpleVelocities)
     nVel = SimpleVelocities(v,w)
     pub.publish(nVel)
 
@@ -31,28 +33,29 @@ def spin():
     global err_angle_prev
     global err_distance_sum
     global err_angle_sum
+
     # 1) call the corresponding service to check if the waypoint was reached
     try:
         get_waypt_reached = rospy.ServiceProxy('check_waypoint_reached', CheckWaypointReached)
-        waypt_reached = get_waypt_reached()
-        #print(waypt_reached)
+        waypt_reached = get_waypt_reached(pose_msg, False)
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
-
-    # 2) subscribe to the robot_curr topic to get the robot's current pose
-    rospy.Subscriber('robot_pose', SimplePoseStamped, get_pose)
+        
+    if waypt_reached.reached:
+        err_distance_prev = 0
+        err_angle_prev = 0
+        err_distance_sum = 0
+        err_angle_sum = 0
+        set_velocity(0,0)
 
     # 3) call the corresponding service to get the current waypoint or waypoint list
     try:
         get_curr_waypt = rospy.ServiceProxy('current_waypoint', CurrentWaypoint)
         curr_waypt = get_curr_waypt()
-        #print(curr_waypt)
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
         
     # 4) implement your PID/PD/P logic
-
-    #def pid_controller(train, g, K_P, K_I, K_D):
     if not curr_waypt.is_empty:
         #Compute target
         target = curr_waypt.goal
@@ -89,22 +92,19 @@ def spin():
         err_distance_prev = 0
         err_angle_prev = 0
         err_distance_sum = 0
-        err_angle_sum =0
+        err_angle_sum = 0
         set_velocity(0,0)
 
-    # 5) publish your computed velocities in the set_velocities topic
-
-    # 6) if there are no waypoints left then set the velocities to 0 and wait for the next waypoint
-
-    # if you are using C++ then leave this function call empty !
     pass
 
 if __name__ == '__main__':
     rospy.init_node('thymio_control_pnode', anonymous=True)
     loop_rate = rospy.Rate(10)
 
+    rospy.Subscriber('robot_pose', SimplePoseStamped, get_pose)
     rospy.wait_for_service('check_waypoint_reached')
     rospy.wait_for_service('current_waypoint')
+    pub = rospy.Publisher('set_velocities', SimpleVelocities)
 
     while not rospy.is_shutdown():
         spin()        
